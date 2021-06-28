54a636d7ea223fee9e05477b65a85595e3cdbd1d
Sarah Drasner <sarah.drasner@gmail.com>
2021-06-28 14:13:24 -0600

    update teal and readme

--- samples/sample2.ts
+++ samples/sample2.ts
@@ -1,33 +1,33 @@
 export default class Definition {
-    constructor(def) {
-        if (invalidParams(def)) {
-            throw new Error('Invalid arguments provided to Lang constructor');
-        }
-
-        this.__def = def instanceof Map ? def : new Map(def);
+  constructor(def) {
+    if (invalidParams(def)) {
+      throw new Error('Invalid arguments provided to Lang constructor')
     }
 
-    clone = () => new Definition(new Map(this.__def));
+    this.__def = def instanceof Map ? def : new Map(def)
+  }
 
-    extend = (def) => {
-        if (invalidParams(def)) {
-            throw new Error(`extend requires Map`);
-        }
+  clone = () => new Definition(new Map(this.__def))
 
-        var variable = 1;
-
-        const extendedLang = new Map(this.__def);
-
-        for (const [key, value] of def) {
-            extendedLang.set(key, value);
-        }
-
-        return new Definition(extendedLang);
+  extend = (def) => {
+    if (invalidParams(def)) {
+      throw new Error(`extend requires Map`)
     }
+
+    var variable = 1
+
+    const extendedLang = new Map(this.__def)
+
+    for (const [key, value] of def) {
+      extendedLang.set(key, value)
+    }
+
+    return new Definition(extendedLang)
+  }
 }
 
 class Foo {
-    static bar = (a, b) => {
-        return null;
-    }
+  static bar = (a, b) => {
+    return null
+  }
 }
