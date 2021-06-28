72faf69ecf43f88d17206cda70e1b6ecb3f538cd
Sarah Drasner <sarah.drasner@gmail.com>
2021-06-28 08:48:26 -0600

    work on glow

---
+++ src/extension.js
@@ -0,0 +1,174 @@
+const path = require('path')
+const fs = require('fs')
+const vscode = require('vscode')
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  const config = vscode.workspace.getConfiguration('fortnite')
+  let disableGlow = config && config.disableGlow ? !!config.disableGlow : false
+
+  let brightness =
+    parseFloat(config.brightness) > 1 ? 1 : parseFloat(config.brightness)
+  brightness = brightness < 0 ? 0 : brightness
+  brightness = isNaN(brightness) ? 0.45 : brightness
+
+  const legendaryBrightness = Math.floor(brightness * 255)
+    .toString(16)
+    .toUpperCase()
+
+  let disposable = vscode.commands.registerCommand(
+    'fortnite.enableLegendary',
+    function () {
+      const isWin = /^win/.test(process.platform)
+      const appDir = path.dirname(require.main.filename)
+      const base = appDir + (isWin ? '\\vs\\code' : '/vs/code')
+
+      const htmlFile =
+        base +
+        (isWin
+          ? '\\electron-browser\\workbench\\workbench.html'
+          : '/electron-browser/workbench/workbench.html')
+
+      const templateFile =
+        base +
+        (isWin
+          ? '\\electron-browser\\workbench\\legendary.js'
+          : '/electron-browser/workbench/legendary.js')
+
+      try {
+        // generate production theme JS
+        const chromeStyles = fs.readFileSync(
+          __dirname + '/css/fortnite-chrome.css',
+          'utf-8'
+        )
+        const jsTemplate = fs.readFileSync(
+          __dirname + '/js/fortnite-template.js',
+          'utf-8'
+        )
+        const themeWithGlow = jsTemplate.replace(
+          /\[DISABLE_GLOW\]/g,
+          disableGlow
+        )
+        const themeWithChrome = themeWithGlow.replace(
+          /\[CHROME_STYLES\]/g,
+          chromeStyles
+        )
+        const finalTheme = themeWithChrome.replace(
+          /\[LEGENDARY_BRIGHTNESS\]/g,
+          legendaryBrightness
+        )
+        fs.writeFileSync(templateFile, finalTheme, 'utf-8')
+
+        // modify workbench html
+        const html = fs.readFileSync(htmlFile, 'utf-8')
+
+        // check if the tag is already there
+        const isEnabled = html.includes('legendary.js')
+
+        if (!isEnabled) {
+          // delete fortnite script tag if there
+          let output = html.replace(
+            /^.*(<!-- FORTNITE --><script src="legendary.js"><\/script><!-- LEGENDARY -->).*\n?/gm,
+            ''
+          )
+          // add script tag
+          output = html.replace(
+            /\<\/html\>/g,
+            `	<!-- FORTNITE --><script src="legendary.js"></script><!-- FORTNITE -->\n`
+          )
+          output += '</html>'
+
+          fs.writeFileSync(htmlFile, output, 'utf-8')
+
+          vscode.window
+            .showInformationMessage(
+              "Legendary enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.",
+              { title: 'Restart editor to complete' }
+            )
+            .then(function (msg) {
+              vscode.commands.executeCommand('workbench.action.reloadWindow')
+            })
+        } else {
+          vscode.window
+            .showInformationMessage(
+              'Legendary is already enabled. Reload to refresh JS settings.',
+              { title: 'Restart editor to refresh settings' }
+            )
+            .then(function (msg) {
+              vscode.commands.executeCommand('workbench.action.reloadWindow')
+            })
+        }
+      } catch (e) {
+        if (/ENOENT|EACCES|EPERM/.test(e.code)) {
+          vscode.window.showInformationMessage(
+            'You must run VS code with admin privileges in order to enable Legendary.'
+          )
+          return
+        } else {
+          vscode.window.showErrorMessage(
+            'Something went wrong when starting legendary. See README for more information.'
+          )
+          return
+        }
+      }
+    }
+  )
+
+  let disable = vscode.commands.registerCommand(
+    'fortnite.disableLegendary',
+    uninstall
+  )
+
+  context.subscriptions.push(disposable)
+  context.subscriptions.push(disable)
+}
+exports.activate = activate
+
+// this method is called when your extension is deactivated
+function deactivate() {
+  // ...
+}
+
+function uninstall() {
+  var isWin = /^win/.test(process.platform)
+  var appDir = path.dirname(require.main.filename)
+  var base = appDir + (isWin ? '\\vs\\code' : '/vs/code')
+  var htmlFile =
+    base +
+    (isWin
+      ? '\\electron-browser\\workbench\\workbench.html'
+      : '/electron-browser/workbench/workbench.html')
+
+  // modify workbench html
+  const html = fs.readFileSync(htmlFile, 'utf-8')
+
+  // check if the tag is already there
+  const isEnabled = html.includes('legendary.js')
+
+  if (isEnabled) {
+    // delete synthwave script tag if there
+    let output = html.replace(
+      /^.*(<!-- FORTNITE --><script src="legendary.js"><\/script><!-- FORTNITE -->).*\n?/gm,
+      ''
+    )
+    fs.writeFileSync(htmlFile, output, 'utf-8')
+
+    vscode.window
+      .showInformationMessage(
+        'Legendary disabled. VS code must reload for this change to take effect',
+        { title: 'Restart editor to complete' }
+      )
+      .then(function (msg) {
+        vscode.commands.executeCommand('workbench.action.reloadWindow')
+      })
+  } else {
+    vscode.window.showInformationMessage("Legendary isn't running.")
+  }
+}
+
+module.exports = {
+  activate,
+  deactivate,
+}
---
+++ src/js/fortnite-template.js
@@ -0,0 +1,101 @@
+;(function () {
+  // Grab body node
+  const bodyNode = document.querySelector('body')
+
+  // Replace the styles with the glow theme
+  const initLegendary = (disableGlow, obs) => {
+    var themeStyleTag = document.querySelector('.vscode-tokens-styles')
+
+    if (!themeStyleTag) {
+      return
+    }
+
+    var initialThemeStyles = themeStyleTag.innerText
+
+    var updatedThemeStyles = initialThemeStyles
+
+    if (!disableGlow) {
+      /* replace red */
+      updatedThemeStyles = updatedThemeStyles.replace(
+        /color: #fe4450;/g,
+        'color: #fff5f6; text-shadow: 0 0 2px #000, 0 0 10px #fc1f2c[LEGENDARY_BRIGHTNESS], 0 0 5px #fc1f2c[LEGENDARY_BRIGHTNESS], 0 0 25px #fc1f2c[LEGENDARY_BRIGHTNESS];'
+      )
+
+      /* replace pink */
+      updatedThemeStyles = updatedThemeStyles.replace(
+        /color: #ff7edb;/g,
+        'color: #f92aad; text-shadow: 0 0 2px #100c0f, 0 0 5px #dc078e33, 0 0 10px #fff3;'
+      )
+
+      /* replace yellow */
+      updatedThemeStyles = updatedThemeStyles.replace(
+        /color: #fdeccc;/g,
+        'color: #efe5d3; text-shadow: 0 0 2px #0e0119, 0 0 8px #ef7b05[LEGENDARY_BRIGHTNESS], 0 0 2px #f3a007[LEGENDARY_BRIGHTNESS];'
+      )
+
+      /* replace green */
+      updatedThemeStyles = updatedThemeStyles.replace(
+        /color: #72f1b8;/g,
+        'color: #72f1b8; text-shadow: 0 0 2px #100c0f, 0 0 10px #257c55[LEGENDARY_BRIGHTNESS], 0 0 35px #212724[LEGENDARY_BRIGHTNESS];'
+      )
+
+      /* replace blue */
+      updatedThemeStyles = updatedThemeStyles.replace(
+        /color: #36f9f6;/g,
+        'color: #fdfdfd; text-shadow: 0 0 2px #001716, 0 0 3px #03edf9[LEGENDARY_BRIGHTNESS], 0 0 5px #03edf9[LEGENDARY_BRIGHTNESS], 0 0 8px #03edf9[LEGENDARY_BRIGHTNESS];'
+      )
+    }
+
+    /* append the remaining styles */
+    updatedThemeStyles = `${updatedThemeStyles}[CHROME_STYLES]`
+
+    const newStyleTag = document.createElement('style')
+    newStyleTag.setAttribute('id', 'fortnite-theme-styles')
+    newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, '')
+    document.body.appendChild(newStyleTag)
+
+    console.log('Fortnite: LEGENDARY initialized!')
+
+    // disconnect the observer because we don't need it anymore
+    if (obs) {
+      obs.disconnect()
+    }
+  }
+
+  // Callback function to execute when mutations are observed
+  const watchForBootstrap = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      // only init if we're using a Fortnite subtheme
+      const isUsingFortnite = document.querySelector(
+        '[class*="sdras-fortnite-vscode-themes"]'
+      )
+      // does the style div exist yet?
+      const tokensLoaded = document.querySelector('.vscode-tokens-styles')
+      // does it have content ?
+      const tokenStyles = document.querySelector(
+        '.vscode-tokens-styles'
+      ).innerText
+
+      if (mutation.type === 'attributes') {
+        // sometimes VS code takes a while to init the styles content, so stop this observer and add an observer for that
+        if (isUsingSynthwave && tokensLoaded) {
+          observer.disconnect()
+          observer.observe(tokensLoaded, { childList: true })
+        }
+      }
+      if (mutation.type === 'childList') {
+        // Everything we need is ready, so initialize
+        if (isUsingFortnite && tokensLoaded && tokenStyles) {
+          initLegendary([DISABLE_GLOW], observer)
+        }
+      }
+    }
+  }
+
+  // try to initialise the theme
+  initLegendary([DISABLE_GLOW])
+
+  // Use a mutation observer to check when we can bootstrap the theme
+  const observer = new MutationObserver(watchForBootstrap)
+  observer.observe(bodyNode, { attributes: true })
+})()
